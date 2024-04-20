69627475b9f546970f168e1551627dedd4b3d78d
OolongTea620 <rlafls9596@gmail.com>
2024-04-20 23:28:06 +0900

    feature: print todo list

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,10 +1,49 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
+    <h1 class="text-center">Todo App</h1>
+    <input 
+      type="text" 
+      class="w-100 p-2" 
+      placeholder="Type todo"
+      @keyup.enter="addTodo"
+    >
+    <hr>
+    <!-- component는 여러개 생성해서 사용 가능 하다 -->
+    <Todo 
+      v-for="todo in todos" 
+      :key="todo.id"
+      :todo="todo"
+    />
   </div>
 </template>
 
 <script>
+import Todo from '@/components/Todo.vue';
 export default {
+  components: {
+    Todo
+  },
+  data() {
+    return {
+      todos: [
+        { id: 1, text: 'buy a car', chacked: false },
+        { id: 2, text: 'eat lunch', chacked: true },
+        { id: 3, text: 'go to sleep', chacked: false },
+      ]
+    }
+  },
+  methods: {
+    addTodo(e) {
+      this.todos.push(
+        {
+          id: Math.random(), 
+          text: e.target.value,
+          chacked: false
+        }
+      )
+      console.log(this.todos)
+    }
+  }
 };
 </script>
 
