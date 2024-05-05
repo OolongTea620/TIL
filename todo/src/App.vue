02832c7905783f9e471c1162d3fd8cfb9151401b
OolongTea620 <rlafls9596@gmail.com>
2024-05-06 00:43:55 +0900

    fix: actions로 비동기로 데이터 불러오기 예제

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -8,6 +8,7 @@
     <!-- component는 여러개 생성해서 사용 가능 하다 -->
     <TodoList />
     <!-- {{ todos }} -->
+    <UserList />
   </div>
 </template>
 
@@ -15,45 +16,14 @@
 import TodoList from '@/components/TodoList.vue';
 import AddTodo from '@/components/AddTodo.vue';
 import CompletedTodo from '@/components/CompletedTodo.vue';
+import UserList from '@/components/UserList.vue';
 
 export default {
   components: {
     TodoList,
     AddTodo,
-    CompletedTodo
-  },
-  data() {
-    return {
-      // todos: [
-      //   { id: 1, text: 'buy a car', checked: false },
-      //   { id: 2, text: 'eat lunch', checked: true },
-      //   { id: 3, text: 'go to sleep', checked: false },
-      // ]
-    }
-  },
-  methods: {
-    // deleteTodo(id) {
-    //   // const index = this.todos.findIndex(todo => {
-    //   //   return todo.id === id;
-    //   // })
-    //   // this.todos.splice(index,1);
-    //   // this.todos = this.todos.filter(todo => todo.id !== id)
-    // },
-    // addTodo(value) {
-    //   this.todos.push(
-    //     {
-    //       id: Math.random(), 
-    //       text: value,
-    //       checked: false
-    //     }
-    //   )
-    // },
-    // toggleCheckbox({id, checked}) {
-    //   const index = this.todos.findIndex(todo => {
-    //     return todo.id === id;
-    //   })
-    //   this.todos[index].checked = checked;
-    // }
+    CompletedTodo,
+    UserList
   }
 };
 </script>
--- todo/src/store/index.js
+++ todo/src/store/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import Vue from 'vue';
 import Vuex from 'vuex';
 
@@ -9,9 +10,13 @@ export default new Vuex.Store({
             { id: 1, text: 'buy a car', checked: false },
             { id: 2, text: 'eat lunch', checked: true },
             { id: 3, text: 'go to sleep', checked: false }
-        ]
+        ],
+        users:[]
     },
     mutations : { // 데이터를 실질적으로 변경 가능함
+        SET_USERS(state, users) {
+            state.users = users;
+        },
         ADD_TODO(state, value) {
             state.todos.push(
                 {
@@ -37,7 +42,12 @@ export default new Vuex.Store({
     actions: { // 비동기 작업 처리
         // 데이터베이스에 새로운 todo를 저장해 달라고 요청 가능
         // 일단은 데이터 베이스 흉내를 내서 구현하자
-
+        getUsers({commit}) {
+            axios.get('https://jsonplaceholder.typicode.com/users')
+                .then(res => {
+                    commit('SET_USERS', res.data);
+            });
+        },
         addTodo({commit}, value){ // context: object, payload: data
             // 여기에 비동기 작업 들어감.
             // 일단 setTimeout 사용
